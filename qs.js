5208fd08703bad20abc9c944a03fc2b610a40140
amanisky <amanisky@126.com>
2020-12-18 12:37:33 +0800

    交互问题

---
+++ qs.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'private',
+    message: '是否为私有仓库'
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: '作者：'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: '项目描述：'
+  }
+]
