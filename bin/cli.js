642a64110de61e7ba68fb98f61e622f6e7b72a0b
amanisky <amanisky@126.com>
2020-12-18 11:03:33 +0800

    init

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,47 @@
 #! /usr/bin/env node
 const { program } = require('commander')
+const { version } = require('../package.json')
 const fn = require('..')
-// 自定义命令
-program.parse(process.argv)
+
+const actionsMap = {
+  'create': {
+    alias: 'crt',
+    desc: 'create a new project',
+    examples: ['my-cli create <name>']
+  },
+  'config': {
+    alias: 'cfg',
+    desc: 'config a file',
+    examples: [
+      'my-cli config set <k> <v>',
+      'my-cli config get <k>'
+    ]
+  }
+}
+
+Reflect.ownKeys(actionsMap).forEach(aName => {
+  const opts = actionsMap[aName]
+  program
+    .command(aName)
+    .alias(opts.alias)
+    .description(opts.desc)
+    .action(function (cmdObj) {
+      fn(aName, cmdObj.args)
+    })
+})
+
+// 自定义帮助信息
+program.on('--help', function () {
+  console.log('\nExamples:')
+  Reflect.ownKeys(actionsMap).forEach(aName => {
+    actionsMap[aName].examples.forEach(item => {
+      console.log(`　${item}`)
+    })
+  })
+})
+
+program
+  .name("my-cli")
+  .description('my-cli tool')
+  .version(version, '-v --vers')
+  .parse(process.argv)
